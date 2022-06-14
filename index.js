5f321ee8796fa3de8b710ba939b33030bf8a0aac
hawk <2813239495@qq.com>
2022-06-14 17:03:52 +0800

    bbb

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 function spawn(genF) {
   return new Promise(function(resolve, reject) {
-    const gen = genF();
+    const gen = genF()
     function step(nextF) {
-      let next;
+      let next
       try {
-        next = nextF();
+        next = nextF()
       } catch(e) {
-        return reject(e);
+        return reject(e)
       }
       if(next.done) {
-        return resolve(next.value);
+        return resolve(next.value)
       }
       Promise.resolve(next.value).then(function(v) {
-        step(function() { return gen.next(v); });
+        step(function() { return gen.next(v) })
       }, function(e) {
-        step(function() { return gen.throw(e); });
-      });
+        step(function() { return gen.throw(e) })
+      })
     }
-    step(function() { return gen.next(undefined); });
-  });
+    step(function() { return gen.next(undefined) })
+  })
+}
+
+async function fn(args) {
+  // ...
+}
+
+// 等同于
+
+function fn(args) {
+  return spawn(function* () {
+    // ...
+  })
 }
